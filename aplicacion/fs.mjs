32f05666fd38919890066fc76badcac2f1756317
kelvin abche <kelvinabache12@gmail.com>
2023-09-06 12:42:53 -0400

    guardando las informacion de la ultima clase

--- aplicacion/fs.mjs
+++ aplicacion/fs.mjs
@@ -1,6 +1,7 @@
 import { Stats } from "node:fs";
 import { readdir, stat } from "node:fs/promises";
 import path from "node:path";
+import pc from "picocolors"; //dependencia de producion 
 
 // readdir nos muestra los directorios que tenemos
 
@@ -12,16 +13,25 @@ async function ls(directorios) {
     files = await readdir(directorios); // recorriendo los archivo que tenemos ahora
   } catch {
     // si tenmos un error al recorrer los archivos
-    console.error("directorio no existe");
+    console.error(pc.red(" X directorio no existe"));
     process.exit(1); // salimos de la peticion
   }
-  const filesPromeses = files.map(async (file) => { //el map esta haciendo en paralelo
+  const filesPromeses = files.map(async (file) => {
+    /*
+    // ver la forma de dependencia o sermantic version
+    x,y,z la x seria la mayor se rompe la compatibilidad hacia atras
+    x,y,z la y seria la menor se anade nuevas funciones 
+    x,y,z la z seria patch es cuando arreglan bugs 
+    
+    
+    */
+    //el map esta haciendo en paralelo
     const filePath = path.join(directorios, file);
     let filesStar;
     try {
       filesStar = await stat(filePath); //retorna la informacion de los archivos
     } catch {
-      console.error("tenemos un error leyendo los archivos");
+      console.error(pc.red("tenemos un error leyendo los archivos"));
     }
 
     const isDirectory = filesStar.isDirectory();
@@ -29,7 +39,7 @@ async function ls(directorios) {
     const fileSize = filesStar.size.toString().padStart(20);
     const fileModified = filesStar.mtime.toLocaleString();
 
-    return `${fileType}-${file.padEnd(20)}${fileSize}/${fileModified}`;
+    return `${fileType}-${pc.blue(file.padEnd(20))}${fileSize}/${pc.bgGreen(fileModified)}`;
   });
   const filesInfo = await Promise.all(filesPromeses);
   filesInfo.forEach((fileInfo) => console.log(fileInfo));
